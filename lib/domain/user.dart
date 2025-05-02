1a7be8a84f4c91f1f69ea8f6ea9ebe2118f9115d
Emiliano Moretta <81765590+emimoretta@users.noreply.github.com>
2025-05-02 20:27:36 -0300

    Update user.dart
    Leer comentarios del archivo

--- lib/domain/user.dart
+++ lib/domain/user.dart
@@ -1,15 +1,18 @@
 enum Role { admin, user, guest }
 
 class User {
+  final String id; //lo tomaríamos la base de datos de Firestore. Es un string
   final String email;
-  final String password;
   final String username;
   final Role role;
 
   User({
+    required this.id, //lo tomaríamos la base de datos de Firestore. Es un string
     required this.email,
-    required this.password,
     required this.username,
     required this.role,
   });
 }
+
+
+//Saqué el password, el profe dijo que no se almacena en el model.
