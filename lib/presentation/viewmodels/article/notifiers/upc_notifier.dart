1754d7a761529e53f3da4ef2af0b2224a2f7c6ce
emimoretta <emimoretta@gmail.com>
2025-05-13 18:42:08 -0300

    agregada las clases para lectura de upc

---
+++ lib/presentation/viewmodels/article/notifiers/upc_notifier.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:inventarium/data/upc_repository.dart';
+import 'package:inventarium/presentation/viewmodels/article/states/upc_state.dart';
+
+final upcNotifierProvider = StateNotifierProvider<UPCNotifier, UPCState>((ref) {
+  return UPCNotifier();
+});
+
+class UPCNotifier extends StateNotifier<UPCState> {
+  final UPCRepository _repository = UPCRepositoryImpl();
+
+  UPCNotifier() : super(const UPCState());
+
+  Future<void> scanUPC(BuildContext context) async {
+    state = state.copyWith(isLoading: true);
+
+    try {
+      final result = await _repository.scanUPC(context);
+      state = state.copyWith(scanResult: result, isLoading: false);
+    } catch (e) {
+      state = state.copyWith(error: e.toString(), isLoading: false);
+    }
+  }
+}
